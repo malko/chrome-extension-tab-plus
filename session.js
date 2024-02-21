c4cdc13d32c2a31f47e44974250932108dfbea06
malko <jgotti@jgotti.org>
2024-02-21 19:20:28 +0100

    ⚡️ (session) Optimize re-rendering of active windows

--- session.js
+++ session.js
@@ -1,5 +1,6 @@
 //@ts-check
 import { WindowWindow, WindowSessionWindow } from "./elements/WindowWindow.js"
+import { WindowTab } from "./elements/WindowTab.js"
 import { SVGs } from "./assets/svgs.js"
 import { getSavedWindows } from "./libs/db.js"
 import { bindPageActions } from "./libs/bindPageActions.js"
@@ -41,12 +42,28 @@ showTitlesEl.addEventListener("change", () => {
 loadingSettings["show-titles"] && rootElmt.classList.add("show-titles")
 //#endregion show titles
 
+const debounced = (fn, delay = 250) => {
+	let lastExec = 0
+	let timer = 0
+	return (...args) => {
+		const now = Date.now()
+		const elapsed = now - lastExec
+		if (elapsed < delay) {
+			clearTimeout(timer)
+			timer = setTimeout(() => {
+				lastExec = now
+				fn(...args)
+			}, delay - elapsed)
+		} else {
+			lastExec = now
+			fn(...args)
+		}
+	}
 }
 const isValidWindowType = (/**@type{{type?:chrome.windows.windowTypeEnum}}*/ { type }) =>
 	type === "normal" || type === "popup"
-const render = async ({ type: evtType, ...evtDetails }) => {
+const renderActiveWindows = debounced(async () => {
 	activesContainer.innerHTML = ""
-	savedContainer.innerHTML = ""
 	await chrome.windows
 		.getAll({ populate: true })
 		.then((windows) => {
@@ -61,6 +78,9 @@ const render = async ({ type: evtType, ...evtDetails }) => {
 			})
 		})
 		.catch(console.error)
+})
+const renderSavedWindows = debounced(async () => {
+	savedContainer.innerHTML = ""
 	await getSavedWindows()
 		.then((windows) => {
 			windows.forEach((win) => {
@@ -69,15 +89,114 @@ const render = async ({ type: evtType, ...evtDetails }) => {
 			})
 		})
 		.catch(console.error)
-	return
+})
+
+const render = async ({ type: evtType, ...evtDetails }) => {
+	renderActiveWindows()
+	renderSavedWindows()
+}
+
+const rerender = async (evt) => {
+	const { type: evtType, ...evtDetails } = evt
+	// avoid to rerender on this tab udpate
+	if (evtType === "tab-updated" && evtDetails.tabId === sessionTabId) {
+		return
+	}
+	const getWinEl = () =>
+		/**@type{WindowWindow|null}*/ (document.querySelector(`window-window#window-${evtDetails.winId}`))
+	const getTabEl = () => getWinEl()?.shadowRoot.querySelector(`window-tab#tab-${evtDetails.tabId}`)
+
+	switch (evtType) {
+		case "window-create":
+			// if the window is a popup or not a normal window, ignore it
+			if (!isValidWindowType(evtDetails)) return
+			return renderActiveWindows()
+		case "window-remove": {
+			const winEl = getWinEl()
+			return winEl ? winEl.remove() : renderActiveWindows()
+		}
+		case "window-focus": {
+			if (evtDetails.winId === -1) return
+			const winEl = getWinEl()
+			if (!winEl) return renderActiveWindows()
+			document.querySelector("window-window.focused")?.classList.remove("focused")
+			return winEl?.classList.add("focused")
+		}
+		case "tab-activated": {
+			if (evtDetails.tabId === sessionTabId) return
+			const tabEl = getTabEl()
+			if (!tabEl) {
+				return renderActiveWindows()
+			}
+			tabEl.parentNode.querySelectorAll("window-tab.active").forEach((el) => el.classList.remove("active"))
+			return tabEl.classList.add("active")
+		}
+		case "tab-detached":
+		case "tab-remove": {
+			const tabEl = getTabEl()
+			return tabEl ? tabEl.remove() : renderActiveWindows()
+		}
+		case "tab-update": {
+			const tabEl = getTabEl()
+			if (tabEl && evtDetails.tab && !evtDetails.groupId) {
+				return tabEl.replaceWith(new WindowTab(evtDetails.tab))
+			} else {
+				return renderActiveWindows()
+			}
+		}
+		case "tab-attached": {
+			const winEl = getWinEl()
+			const tab = await chrome.tabs.get(evtDetails.tabId)
+			return winEl && tab ? winEl.appendTab(tab) : renderActiveWindows()
+		}
+		case "tab-create": {
+			const winEl = getWinEl()
+			return winEl ? winEl.appendTab(evtDetails.tab) : renderActiveWindows()
+		}
+		case "tab-move": {
+			const tabEl = getTabEl()
+			if (!tabEl) return renderActiveWindows()
+			const { fromIndex, toIndex } = evtDetails.moveInfo
+			if (toIndex === 0) {
+				return tabEl.parentNode.prepend(tabEl)
+			} else if (fromIndex < toIndex) {
+				// move to the right
+				return tabEl.parentNode.insertBefore(tabEl, tabEl.parentNode.children[toIndex + 1])
+			} else {
+				// move to the left
+				return tabEl.parentNode.insertBefore(tabEl, tabEl.parentNode.children[toIndex])
+			}
+		}
+		case "tab+session-window-saved":
+			return renderSavedWindows()
+		default:
+			console.error("Unknown render event type", evtType, evtDetails)
+	}
+	render({ type: evtType, ...evtDetails })
 }
 
 render({ type: "init" })
 chrome.windows.onCreated.addListener((win) => rerender({ type: "window-create", winId: win.id }))
 chrome.windows.onRemoved.addListener((winId) => rerender({ type: "window-remove", winId }))
 chrome.windows.onFocusChanged.addListener((winId) => rerender({ type: "window-focus", winId }))
-chrome.tabs.onCreated.addListener((tab) => rerender({ type: "tab-create", tabId: tab.id }))
-chrome.tabs.onRemoved.addListener((tabId, removeInfo) => rerender({ type: "tab-remove", tabId }))
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => rerender({ type: "tab-update", tabId }))
+chrome.tabs.onCreated.addListener((tab) => rerender({ type: "tab-create", tabId: tab.id, tab, winId: tab.windowId }))
+chrome.tabs.onRemoved.addListener((tabId, removeInfo) =>
+	rerender({ type: "tab-remove", tabId, winId: removeInfo.windowId, isWindowClosing: removeInfo.isWindowClosing })
+)
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) =>
+	rerender({ type: "tab-update", tabId, winId: tab.windowId, tab, changeInfo })
+)
+chrome.tabs.onMoved.addListener((tabId, moveInfo) =>
+	rerender({ type: "tab-move", tabId, winId: moveInfo.windowId, moveInfo })
+)
+chrome.tabs.onDetached.addListener((tabId, detachInfo) =>
+	rerender({ type: "tab-detached", tabId, winId: detachInfo.oldWindowId, oldPosition: detachInfo.oldPosition })
+)
+chrome.tabs.onAttached.addListener((tabId, attachInfo) =>
+	rerender({ type: "tab-attached", tabId, winId: attachInfo.newWindowId, newPosition: attachInfo.newPosition })
+)
+chrome.tabs.onActivated.addListener((activeInfo) =>
+	rerender({ type: "tab-activated", tabId: activeInfo.tabId, winId: activeInfo.windowId })
+)
 window.addEventListener("tab+session-window-saved", rerender)
 export {}
