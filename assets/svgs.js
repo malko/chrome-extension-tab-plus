eea8d95b04273556eac20bc2b21895c101d45cf3
malko <jgotti@jgotti.org>
2024-02-22 17:59:31 +0100

    💄 (svg) update X icon

--- assets/svgs.js
+++ assets/svgs.js
@@ -1,3 +1,4 @@
+const svg24 = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">'
 const stroke = `stroke:var(--svg-color, #000);`
 const fill = `fill:var(--svg-color, #000);`
 const roundStroke = `${stroke}stroke-linecap:round;stroke-linejoin:round;`
@@ -12,7 +13,7 @@ export const SVGs = {
 <path class="cls1" style="fill:var(--svg-color, #000);" d="m16.5 2v3.5h3.5z"/>
 </svg>`,
 
-	checkmark: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+	checkmark: `${svg24}
 	<path style="${roundStroke}stroke-width:4;${noFill}" d="m5 12l4 6l11-12">
 </svg>`,
 
@@ -27,7 +28,7 @@ export const SVGs = {
 <path class="cls1" d="M9 5C9 3.89543 9.89543 3 11 3H13C14.1046 3 15 3.89543 15 5V7H9V5Z" />
 </svg>`,
 
-	earth: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+	earth: `${svg24}
 	<defs><style>.cls1{${fill}${roundStroke}stroke-width:2px;}</style></defs>
 	<circle class="cls1" style="${noFill}" cx="12" cy="12" r="9" />
 	<path class="cls1" d="m4 10h2l3 1l-2 2l3 2l-4 3.5z"/>
@@ -44,7 +45,7 @@ export const SVGs = {
 <rect width="16.099" height="16.785" x="4" y="27.215" rx="2.552" ry="2.466" class="cls1"/>
 </svg>`,
 
-	incognito: `<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+	incognito: `${svg24}
 <defs>
 <style>.cls-1{${noFill}${stroke};stroke-miterlimit:10;stroke-width:2;}</style>
 </defs>
@@ -73,13 +74,13 @@ export const SVGs = {
 </g>
 </svg>`,
 
-	maximize: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+	maximize: `${svg24}
 <path
 	style="stroke-width:2px;${roundStroke}${noFill}"
 	d="m2 14v8h8m-8 0l6-6m3-3l11-11h-8m8 0v8"
 /></svg>`,
 
-	minimize: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+	minimize: `${svg24}
 <path
 	style="stroke-width:2px;${roundStroke}${noFill}"
 	d="m14 2v8h8m-8 0l6-6m-16 16l6-6h-8m8 0v8"
@@ -91,25 +92,23 @@ export const SVGs = {
 </svg>
 `,
 
-	no: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+	no: `${svg24}
   <path style="${noFill}${roundStroke}stroke-width:4;" d="m5 5l14 14m-14 0l14 -14" transform="matrix(.8 0 -.2 1 5 0)">
 </svg>
 `,
 
-	offline: `<?xml version="1.0" ?>
-<svg viewBox="0 0 24 24" version="1.1" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+	offline: `${svg24}
 <path style="${fill}" d="M2,14.5c0,1.6,0.9,3.1,2.2,3.9l-0.9,0.9c-0.4,0.4-0.4,1,0,1.4C3.5,20.9,3.7,21,4,21s0.5-0.1,0.7-0.3l14-14   c0.4-0.4,0.4-1,0-1.4s-1-0.4-1.4,0l-1.8,1.8C14.9,7,14.3,7,13.8,7c-0.1-0.2-0.3-0.3-0.4-0.4C12.4,5.6,11,5,9.5,5S6.6,5.6,5.6,6.6   C4.6,7.6,4,9,4,10.5c0,0.1,0,0.2,0,0.3c-0.2,0.2-0.5,0.3-0.7,0.6C2.5,12.2,2,13.3,2,14.5z M4.7,12.7c0.2-0.2,0.5-0.4,0.7-0.5   c0.4-0.2,0.7-0.6,0.6-1.1C6,10.9,6,10.7,6,10.5C6,9.6,6.4,8.7,7,8c1.3-1.3,3.6-1.3,4.9,0c0.2,0.2,0.4,0.4,0.5,0.7   c0.2,0.3,0.6,0.5,1,0.4l-7.7,7.7c-1-0.3-1.7-1.3-1.7-2.4C4,13.8,4.3,13.2,4.7,12.7z"/>
 <path style="${fill}" d="M19.6,10.6c-0.2-0.4-0.4-0.8-0.6-1.2c-0.3-0.5-0.9-0.6-1.4-0.3c-0.5,0.3-0.6,0.9-0.3,1.4c0.2,0.3,0.4,0.7,0.5,1   c0.1,0.3,0.3,0.5,0.6,0.7c0.9,0.4,1.5,1.3,1.5,2.3c0,0.7-0.3,1.3-0.7,1.8c-0.5,0.5-1.1,0.7-1.8,0.7H10c-0.6,0-1,0.4-1,1s0.4,1,1,1   h7.5c1.2,0,2.3-0.5,3.2-1.3c0.9-0.8,1.3-2,1.3-3.2C22,12.8,21.1,11.3,19.6,10.6z"/>
 </svg>`,
 
-	online: `<?xml version="1.0" ?>
-<svg viewBox="0 0 24 24" version="1.1" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+	online: `${svg24}
 <path style="${fill}" d="M13.3,16.3L13,16.6V13c0-0.6-0.4-1-1-1s-1,0.4-1,1v3.6l-0.3-0.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l2,2    c0.1,0.1,0.2,0.2,0.3,0.2C11.7,20,11.9,20,12,20s0.3,0,0.4-0.1c0.1-0.1,0.2-0.1,0.3-0.2l2-2c0.4-0.4,0.4-1,0-1.4    S13.7,15.9,13.3,16.3z"/>
 <path style="${fill}" d="M19.6,9.6c-0.3-0.7-0.7-1.4-1.3-1.9c-1.2-1.2-2.9-1.8-4.6-1.6c-0.1-0.2-0.3-0.3-0.4-0.4C12.4,4.6,11,4,9.5,4    S6.6,4.6,5.6,5.6C4.6,6.6,4,8,4,9.5c0,0.1,0,0.2,0,0.3c-0.2,0.2-0.5,0.3-0.7,0.6c-0.9,0.8-1.3,2-1.3,3.2C2,16,4,18,6.5,18    c0.6,0,1-0.4,1-1s-0.4-1-1-1C5.1,16,4,14.9,4,13.5c0-0.7,0.3-1.3,0.7-1.8c0.2-0.2,0.5-0.4,0.7-0.5c0.4-0.2,0.7-0.6,0.6-1.1    C6,9.9,6,9.7,6,9.5C6,8.6,6.4,7.7,7,7c1.3-1.3,3.6-1.3,4.9,0c0.2,0.2,0.4,0.4,0.5,0.7c0.2,0.4,0.7,0.5,1.1,0.4    C13.9,8,14.2,8,14.5,8c0.9,0,1.8,0.4,2.5,1c0.4,0.4,0.7,0.9,0.9,1.5c0.1,0.3,0.3,0.5,0.6,0.7c0.9,0.4,1.5,1.3,1.5,2.3    c0,0.7-0.3,1.3-0.7,1.8c-0.5,0.5-1.1,0.7-1.8,0.7c-0.6,0-1,0.4-1,1s0.4,1,1,1c1.2,0,2.3-0.5,3.2-1.3c0.9-0.8,1.3-2,1.3-3.2    C22,11.8,21.1,10.3,19.6,9.6z"/>
 </svg>`,
 
-	quit: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-<path style="${roundStroke}stroke-width:2px" d="M15 16.5V19a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h7a2 2 0 0 1 2 2v3.063M11 12h10m0 0-2.5-2.5M21 12l-2.5 2.5"/>
+	quit: `${svg24}
+<path style="${noFill}${roundStroke}stroke-width:2px" d="M15 16.5V19a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h7a2 2 0 0 1 2 2v3.063M11 12h10m0 0-2.5-2.5M21 12l-2.5 2.5"/>
 </svg>`,
 
 	reload: `<?xml version="1.0" encoding="utf-8"?>
@@ -133,12 +132,12 @@ export const SVGs = {
 <path style="${stroke}${noFill}" stroke-width="1.176" d="M16 14.75a1.75 1.75 0 1 0 0-3.5 1.75 1.75 0 0 0 0 3.5Zm-8-2a1.75 1.75 0 1 0 0-3.5 1.75 1.75 0 0 0 0 3.5Z"/>
 </svg>`,
 
-	tabNew: `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+	tabNew: `${svg24}
 <path d="M3 6C3 4.34315 4.34315 3 6 3H18C19.6569 3 21 4.34315 21 6V12.0218C20.3945 11.6341 19.7204 11.3441 19 11.1739V6C19 5.44772 18.5523 5 18 5H6C5.44772 5 5 5.44772 5 6V18C5 18.5523 5.44772 19 6 19H11.1739C11.3441 19.7204 11.6341 20.3945 12.0218 21H6C4.34315 21 3 19.6569 3 18V6Z" fill="var(--svg-color, #212121)"/>
 <path d="M23 17.5C23 14.4624 20.5376 12 17.5 12C14.4624 12 12 14.4624 12 17.5C12 20.5376 14.4624 23 17.5 23C20.5376 23 23 20.5376 23 17.5ZM18.0006 18L18.0011 20.5035C18.0011 20.7797 17.7773 21.0035 17.5011 21.0035C17.225 21.0035 17.0011 20.7797 17.0011 20.5035L17.0006 18H14.4961C14.22 18 13.9961 17.7762 13.9961 17.5C13.9961 17.2239 14.22 17 14.4961 17H17.0005L17 14.4993C17 14.2231 17.2239 13.9993 17.5 13.9993C17.7761 13.9993 18 14.2231 18 14.4993L18.0005 17H20.503C20.7792 17 21.003 17.2239 21.003 17.5C21.003 17.7762 20.7792 18 20.503 18H18.0006Z" fill="var(--svg-color, #212121)"/>
 </svg>`,
 
-	tabNewIncognito: `<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+	tabNewIncognito: `${svg24}
 	<defs><style>
 		.cls-1{${noFill}${stroke}stroke-miterlimit:10;stroke-width:1.92px;}
 		.cls-2{${fill}}
@@ -156,22 +155,19 @@ export const SVGs = {
 	</g>
 	</svg>`,
 
-	save: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+	save: `${svg24}
 <path d="M1.874 14.172v5.84l18.5-.022-.028-5.862" style="${roundStroke}${noFill}stroke-width:2.98072;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" transform="translate(.876 1.182)"/>
 <path d="M11.193 1.623s1.74 4.026 1.615 6.724c-.193 3.15-1.66 6.73-1.66 6.73" style="${roundMiterStroke}${noFill}stroke-width:2.98072;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" transform="translate(.876 1.182)"/>
 <path d="M15.892 12.851c.057.897-5.694 4.947-6.453 4.542-.76-.403-1.232-7.766-.532-8.258.702-.494 6.926 2.818 6.984 3.715Z" style="${fill}fill-opacity:1;stroke-width:2.49068;${roundStroke}stroke-miterlimit:4;stroke-dasharray:none;paint-order:stroke fill markers;stop-color:var(--svg-color,#000)" transform="translate(.876 1.182)"/>
 </svg>`,
 
-	sun: `<svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" id="Icons" version="1.1" viewBox="0 0 24 24">
+	sun: `${svg24}
 <style>.cls1{${noFill}stroke-width:2;${roundStroke}stroke-miterlimit:10}</style>
 <circle cx="12" cy="12" r="5" class="cls1"/>
 <path d="m12 2v4m0,12v4 m-10-10h4m12 0h4m-4 6 l2 2m-16 0l2-2 m0-12l-2-2m16 0l-2 2" class="cls1"/>
 </svg>`,
-
-	x: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-<defs><style>.cls1{fill-opacity:1;stroke-width:1.80075;${roundStroke}paint-order:stroke fill markers;stop-color:var(--svg-color,#000)}</style></defs>
-<path class="cls1" d="m4 11h16m-8-8v16" transform="matrix(.6109 .7071 -.80331 .7071 13.786 -4.315)"/>
-</svg>`,
-
+	get x() {
+		return this.no
+	},
 	github: `<svg  viewBox="0 0 98 96" xmlns="http://www.w3.org/2000/svg"><path  style="fill:var(--svg-color,#24292f)" fill-rule="evenodd" clip-rule="evenodd" d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"/></svg>`,
 }
